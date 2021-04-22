ef70cd0c72721f27de5ef50262c18256308ffefa
Olivier Babiard <olivier.babiard@gmail.com>
2021-04-22 22:44:52 +0200

    v0

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,51 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = pickedCards;
+    this.pairsClicked = pairsClicked;
+    this.pairsGuessed = pairsGuessed;
   }
-  shuffleCards() {}
+
+  shuffleCards(cards) {
+    if (this.cards !== cardsArray) {
+      return undefined;
+    } else {
+      const shuffledCards = [];
+      console.log(shuffledCards)
+      var m = shuffledCards.length, t, i;
+      while (m) {
+          i = Math.floor(Math.random() * m--);
+          t = shuffledCards[m];
+          shuffledCards[m] = shuffledCards[i];
+          shuffledCards[i] = t;
+        }
+      return shuffledCards;
+    } 
+  }
+
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
+
+
+
+const cardsArray = [
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" }
+]; 
+
+console.log(cardsArray)
